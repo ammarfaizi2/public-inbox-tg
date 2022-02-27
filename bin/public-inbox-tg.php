ammarfaizi2/public-inbox-tg

commit 2d8a719c4c724018c6389bdda8796587ca01145b

bin/public-inbox-tg: Reflect the replied message on Telegram
Email can have In-Reply-To header that represents to which another
email it's replying to. Use this identifier to reflect the reply to
message on Telegram. Use simple file database to track the relationship
between Telegram message ID and email message ID.

Signed-off-by: Alviro Iskandar Setiawan <alviro.iskandar@gnuweeb.org>
Link: https://t.me/GNUWeeb/544196
Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

bin/public-inbox-tg.php
@@ -39,6 +39,27 @@ function listTelegramLookup(array $list): array
 	return $ret;
 }
 
+function tgMsgIdInsert(string $msgId, int $tgMsgId): bool
+{
+	if (!is_dir(DATA_DIR))
+		mkdir(DATA_DIR);
+
+	if (!is_dir(DATA_DIR."/tg_msg_id"))
+		mkdir(DATA_DIR."/tg_msg_id");
+
+	$file = DATA_DIR."/tg_msg_id/".sha1($msgId);
+	return (bool) file_put_contents($file, (string) $tgMsgId);
+}
+
+function tgMsgIdLookup(string $msgId): int
+{
+	$file = DATA_DIR."/tg_msg_id/".sha1($msgId);
+	if (!file_exists($file))
+		return 0;
+
+	return (int) file_get_contents($file);
+}
+
 function extractList(string $str): array
 {
 	$str = explode(",", $str);
@@ -132,7 +153,15 @@ function fx(string $input): int
 	}
 	$msgId = $m[1];
 	if ($msgId[0] === '<' && $msgId[strlen($msgId) - 1] === '>')
-		$msgId = substr(clean_header_val($m[1]), 1, -1);
+		$msgId = substr(clean_header_val($msgId), 1, -1);
+
+	if (preg_match("/(?:^|\\n)in-reply-to:\s+?(.+?)(?:\\n\S+\:|\\n\\n)/si", $hdr, $m)) {
+		$inReplyTo = $m[1];
+		if ($inReplyTo[0] === '<' && $inReplyTo[strlen($inReplyTo) - 1] === '>')
+			$inReplyTo = substr(clean_header_val($inReplyTo), 1, -1);
+	} else {
+		$inReplyTo = NULL;
+	}
 
 	$err = "";
 	if (preg_match("/(?:^|\\n)to:\s+?(.+?)(?:\\n\S+\:|\\n\\n)/si", $hdr, $m)) {
@@ -181,6 +210,8 @@ function fx(string $input): int
 		]
 	];
 
+	$replyToTgMsgId = $inReplyTo ? tgMsgIdLookup($msgId) : 0;
+
 	if (preg_match("/\[.*(?:patch|rfc).*?(?:(\d+)\/(\d+))?\](.+)/i", $subject, $m) &&
 	    preg_match("/diff --git/", $body)) {
 
@@ -191,15 +222,15 @@ function fx(string $input): int
 		if (!$n)
 			$n = 1;
 
-		$file = sprintf("%s/%04d-%s.patch", $tmpDir, $n,
-				substr(slugify(trim($m[3])), 0, 40));
+		$file = sprintf("%s/%04d-%s.patch", $tmpDir, $n, substr(slugify(trim($m[3])), 0, 40));
 		file_put_contents($file, $input);
 
-		sendFile([
+		$o = sendFile([
 			"chat_id" => TARGET_CHAT_ID,
 			"document" => new \CurlFile($file),
 			"caption" => "#patch ".$msg,
-			"reply_markup" => json_encode($replyMarkup)
+			"reply_markup" => json_encode($replyMarkup),
+			"reply_to_message_id" => $replyToTgMsgId
 		]);
 		unlink($file);
 		rmdir($tmpDir);
@@ -211,13 +242,18 @@ function fx(string $input): int
 		$msg = htmlspecialchars($msg, ENT_QUOTES, "UTF-8");
 		$msg .= "\n<code>------------------------------------------------------------------------</code>";
 
-		sendMessage($msg, TARGET_CHAT_ID,
+		$o = sendMessage($msg, TARGET_CHAT_ID,
 			[
 				"parse_mode" => "HTML",
-				"reply_markup" => $replyMarkup
+				"reply_markup" => $replyMarkup,
+				"reply_to_message_id" => $replyToTgMsgId
 			]
 		);
 	}
+
+	if (isset($o["result"]["message_id"]))
+		tgMsgIdInsert($msgId, $o["result"]["message_id"]);
+
 out:
 	if ($err)
 		echo $err;

commit 12465986f93af752921c672e32ef48cdc0652d1c

bin/public-inbox-tg: Fix tgMsgIdLookup() argument
The argument to tgMsgIdLookup() was wrong, it should be $inReplyTo,
not $msgId, because that's the message we're going to reply.

Fixes: 2d8a719c4c724018c6389bdda8796587ca01145b ("bin/public-inbox-tg: Reflect the replied message on Telegram")
Signed-off-by: Alviro Iskandar Setiawan <alviro.iskandar@gnuweeb.org>
Link: https://lore.gnuweeb.org/gwml/20220227190849.3908933-2-alviro.iskandar@gnuweeb.org
Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

bin/public-inbox-tg.php
@@ -210,7 +210,7 @@ function fx(string $input): int
 		]
 	];
 
-	$replyToTgMsgId = $inReplyTo ? tgMsgIdLookup($msgId) : 0;
+	$replyToTgMsgId = $inReplyTo ? tgMsgIdLookup($inReplyTo) : 0;
 
 	if (preg_match("/\[.*(?:patch|rfc).*?(?:(\d+)\/(\d+))?\](.+)/i", $subject, $m) &&
 	    preg_match("/diff --git/", $body)) {