ammarfaizi2/public-inbox-tg

commit def1707bd7768ed0f7f3a7fed6ddb8aa647f539b

bin/public-inbox-tg: Replace tab char with 8 spaces
The message on Telegram is usually broken when we have tab characters.
So replace it with 8 spaces for better result.

Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

bin/public-inbox-tg.php
@@ -178,6 +178,7 @@ function fx(string $input): int
 		$ccListStr = "";
 	}
 
+	$content = str_replace("\t", "        ", $content);
 	$content = trim(substr($body, 0, MAX_BODYLEN));
 	$msg = "#ml\nFrom: {$from}\n";
 

commit 11f2f1096baee5589117c9ae5fc803cbccebee46

bin/public-inbox-tg: Fix undefined variable `$content`
Fix the following warning:

  PHP Warning:  Undefined variable $content

It's introduced by commit def1707bd77 ("bin/public-inbox-tg: Replace
tab char with 8 spaces").

Fixes: def1707bd7768ed0f7f3a7fed6ddb8aa647f539b ("bin/public-inbox-tg: Replace tab char with 8 spaces")
Signed-off-by: Alviro Iskandar Setiawan <alviro.iskandar@gnuweeb.org>
Link: https://lore.gnuweeb.org/gwml/20220305173617.3605062-3-alviro.iskandar@gnuweeb.org
Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

bin/public-inbox-tg.php
@@ -178,8 +178,8 @@ function fx(string $input): int
 		$ccListStr = "";
 	}
 
-	$content = str_replace("\t", "        ", $content);
-	$content = trim(substr($body, 0, MAX_BODYLEN));
+	$content = str_replace("\t", "        ", $body);
+	$content = trim(substr($content, 0, MAX_BODYLEN));
 	$msg = "#ml\nFrom: {$from}\n";
 
 	if ($toListStr)